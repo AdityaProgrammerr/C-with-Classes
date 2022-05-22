502149fabd97c8cbd2e45671933d08e15754f796
AdityaProgrammerr <106011806+AdityaProgrammerr@users.noreply.github.com>
2022-05-22 04:21:58 -0700

    Add files via upload

---
+++ New.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  
+{
+    intArr[10];
+
+    int *p = NULL;
+
+   // p = (int*)malloc(sizeof(int)*10);
+ 
+    P = new int[10];
+    return 0;
+ }
